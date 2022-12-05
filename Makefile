6e714bb4305ebde1d40dc11a9966f7d2893f7baf
Anton Semenov <s.anton@live.ru>
2022-12-05 10:36:00 +0300

    added dockerfile

--- Makefile
+++ Makefile
@@ -10,4 +10,10 @@ lint:
 start:
 	poetry run uvicorn main:app --reload
 
-.PHONY: test
+docker:
+	docker run -p 80:80 -v bot_db:/code/db/ --rm ssant/catbread
+
+build:
+	docker build -t ssant/catbread .
+
+.PHONY: test 
--- bot_api/database.py
+++ bot_api/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = 'sqlite:///./bot.db'
+DATABASE_URL = 'sqlite:///./db/sample.db'
 
 engine = create_engine(
     DATABASE_URL, connect_args={'check_same_thread': False}
